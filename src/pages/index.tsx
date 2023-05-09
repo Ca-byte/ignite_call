f25e92a0c14c9595e12245071df8fb6004f090da
Caroline Vieira <carolinesanvieira@gmail.com>
2023-05-09 19:37:29 +0200

    chore: Configure ESLint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,5 @@
-import { Heading, Text} from '@ignite-ui/react'
+import { Heading } from '@ignite-ui/react'
 
-export default function Home(){
-	return(
-		<Heading as="h1"> Hello Peach!</Heading>
-	)
+export default function Home() {
+  return <Heading as="h1"> Hello Peach!</Heading>
 }
