2e5949cdcfd377c56828e63a65846800624b61c3
Caroline Vieira <carolinesanvieira@gmail.com>
2023-05-10 22:27:32 +0200

    feat: User registration - Home Visual structure

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,29 @@
+import { Heading, Text } from '@ignite-ui/react'
+import { Container, Hero, Preview } from './style'
+import Image from 'next/image'
+import previewImage from '../../assets/app-preview.png'
+
+export default function Home() {
+  return (
+    <Container>
+      <Hero>
+        <Heading as="h1" size="4xl">
+          Hassle-free scheduling
+        </Heading>
+        <Text size="xl">
+          Connect your calendar and let people book appointments in their free
+          time.
+        </Text>
+      </Hero>
+      <Preview>
+        <Image
+          src={previewImage}
+          height={400}
+          quality={100}
+          priority
+          alt="Calendar"
+        />
+      </Preview>
+    </Container>
+  )
+}
