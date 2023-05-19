bf7cf25ee66929d440acb2ae9b201da159af32cb
Caroline Vieira <carolinesanvieira@gmail.com>
2023-05-19 22:39:14 +0200

    feat: Calendar Connection - Calendar Connection page

---
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -0,0 +1,32 @@
+import { Button, Heading, MultiStep, Text } from '@ignite-ui/react'
+import { Container, Header } from '../styles'
+import { ArrowRight } from 'phosphor-react'
+import { ConnectBox, ConnectItem } from './styles'
+
+export default function Register() {
+  return (
+    <Container>
+      <Header>
+        <Heading as="strong">Connect your schedule!</Heading>
+        <Text>
+          Connect your calendar to automatically check for peak times and new
+          events as scheduled.
+        </Text>
+        <MultiStep size={4} currentStep={2} />
+      </Header>
+      <ConnectBox>
+        <ConnectItem>
+          <Text>Google Calendar</Text>
+          <Button variant="secondary" size="sm">
+            Connect
+            <ArrowRight />
+          </Button>
+        </ConnectItem>
+        <Button type="submit">
+          Next step
+          <ArrowRight />
+        </Button>
+      </ConnectBox>
+    </Container>
+  )
+}
