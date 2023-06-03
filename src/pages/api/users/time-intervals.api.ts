c3aa345e6d1c17ea69a9c03042418f9aef0660d2
Caroline Vieira <carolinesanvieira@gmail.com>
2023-06-03 19:32:24 +0200

    feat: Configuring availability - persistent intervals

--- src/pages/api/users/time-intervals.api.ts
+++ src/pages/api/users/time-intervals.api.ts
@@ -1,6 +1,18 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { getServerSession } from 'next-auth'
 import { buildNextAuthOptions } from '../auth/[...nextauth].api'
+import { z } from 'zod'
+import { prisma } from '@/src/lib/prisma'
+
+const timeIntervalBodySchema = z.object({
+  intervals: z.array(
+    z.object({
+      weekDay: z.number(),
+      startTimeInMinutes: z.number(),
+      endTimeInMinutes: z.number(),
+    }),
+  ),
+})
 
 export default async function handler(
   req: NextApiRequest,
@@ -20,7 +32,20 @@ export default async function handler(
     return
   }
 
-  return res.json({
-    session,
-  })
+  const { intervals } = timeIntervalBodySchema.parse(req.body)
+
+  await Promise.all(
+    intervals.map((interval) => {
+      return prisma.userTimeInterval.create({
+        data: {
+          week_day: interval.weekDay,
+          time_start_in_minutes: interval.startTimeInMinutes,
+          time_end_in_minutes: interval.endTimeInMinutes,
+          user_id: session.user?.id,
+        },
+      })
+    }),
+  )
+
+  return res.status(201).end()
 }
