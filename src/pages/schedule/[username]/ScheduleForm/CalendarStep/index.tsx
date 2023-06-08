02926e2e66b36bc33c24916828e89b01f96e6455
Caroline Vieira <carolinesanvieira@gmail.com>
2023-06-08 20:29:10 +0200

    feat:Calendar and Availability - Timetable  Visual structure

--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -1,10 +1,37 @@
 import { Calendar } from '@/src/components/Calendar'
-import { Container } from './styles'
+import {
+  Container,
+  TimePicker,
+  TimePickerHeader,
+  TimePickerItem,
+  TimePickerList,
+} from './styles'
 
 export function CalendarStep() {
+  const isDateSelected = true
   return (
-    <Container>
+    <Container isTimePickerOpen={isDateSelected}>
       <Calendar />
+      {isDateSelected && (
+        <TimePicker>
+          <TimePickerHeader>
+            Thurday <span>February 12th</span>
+          </TimePickerHeader>
+          <TimePickerList>
+            <TimePickerItem>09:00</TimePickerItem>
+            <TimePickerItem>10:00</TimePickerItem>
+            <TimePickerItem>11:00</TimePickerItem>
+            <TimePickerItem>19:00</TimePickerItem>
+            <TimePickerItem>20:00</TimePickerItem>
+            <TimePickerItem>00:00</TimePickerItem>
+            <TimePickerItem>09:00</TimePickerItem>
+            <TimePickerItem>07:00</TimePickerItem>
+            <TimePickerItem>06:00</TimePickerItem>
+            <TimePickerItem>05:00</TimePickerItem>
+            <TimePickerItem>03:00</TimePickerItem>
+          </TimePickerList>
+        </TimePicker>
+      )}
     </Container>
   )
 }
