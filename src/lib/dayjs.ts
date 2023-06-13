8e8834fea178fc1db33a55159feaec89bc3e1709
Caroline Vieira <carolinesanvieira@gmail.com>
2023-06-13 18:31:10 +0200

    feat: Calendar and Availability - Month navigation

---
+++ src/lib/dayjs.ts
@@ -0,0 +1,6 @@
+// We are current use by default in case to change to portugueses import this file on _app.pages.tsx
+
+import dayjs from 'dayjs'
+
+import 'dayjs/locale/pt-br'
+dayjs.locale('pt-br')
